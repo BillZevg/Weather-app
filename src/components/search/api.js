23e6b2714b54f99418f6dcf0c621fff22978d902
Bill Zevgitis <128227369+BillZevg@users.noreply.github.com>
2024-03-05 21:25:24 +0200

    Add files via upload

---
+++ src/components/search/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b9b3e40170msh5f12afaf374d938p1d593ejsnc66b879980ab",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8bd3c92ba1ea4d4a220b6ec2ecb65b1b";
